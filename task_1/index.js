8d9bdb902ec02c9f9b92eb77bddf475c0ba0ae7a
adpol95 <adpol95@gmail.com>
2024-04-23 19:13:48 +0300

    Завершены задачи 1 и 2

--- task_1/index.js
+++ task_1/index.js
@@ -2,7 +2,26 @@ import {encoded, translations} from './data.js'
 
 console.log("Let's rock")
 console.log(encoded, translations)
+const arrGroupId = [];
+const decoded = encoded.map(el => {
+    for (let key in el) {
+        if (key.endsWith("Id") && typeof el[key] !== "number" && el[key] !== null) el[key] = translations[el[key]];
+    }
+    return el;
+});
+
+const prepareUniq = decoded.map(el => {
+    const str = JSON.stringify(el);
+    return "{" + str.slice(str.indexOf(',') + 1);
+})
+
+const uniqIds = prepareUniq
+    .filter((el, i) => {
+        const compare = i === prepareUniq.indexOf(el);
+        if (compare) arrGroupId.push(i);
+        return compare;
+    })
+    .map((el, i) => JSON.parse(el.replace('{', '{"groupId":' + (+arrGroupId[i] + 783) + ",")))
 
 
-
-// console.log(decoded)
+console.log(decoded, uniqIds)
--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,133 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  let selected: any;
+  let base = {
+    answer: 1,
+    ready: 1
+  }
+
+  const getterApi = (async () => {
+    const arr: any = [];
+    await fetch("https://open.er-api.com/v6/latest/USD")
+            .then(res => res.json())
+            .then(data => {
+              for (let key in data.rates) {
+                if (key !== "USD") arr.push({
+                  id: Math.floor(Math.random() * 10000) + 1,
+                  cur: key,
+                  value: data.rates[key]
+                })
+              }
+            })
+            .catch(err => console.log(err));
+    return arr;
+  })();
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <h1>Конвертер валюты</h1>
+  <div class="converter-main">
+    <div class="titles">
+      <div class="titles__usd">USD</div>
+      <div class="titles__result">
+        {#await getterApi}
+          <p>...waiting</p>
+        {:then data}
+          <form>
+            <select
+                    bind:value={selected}
+                    on:change={() => {
+                            base.answer = +(selected.value * base.ready).toFixed(3);
+                        }}
+            >
+              {#each data as currency}
+                <option value={currency}>
+                  {currency.cur}
+                </option>
+              {/each}
+            </select>
+          </form>
+        {/await}
+      </div>
+    </div>
+    <div class="inputs">
+      <input bind:value={base.ready} on:input={() => {
+            base.answer = +(base.ready * selected.value).toFixed(3);
+        }} type="number"/>
+      <input bind:value={base.answer} on:input={() => {
+            base.ready = +(base.answer / selected.value).toFixed(3);
+        }} type="number"/>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    row-gap: 2em;
+    background: radial-gradient(99.45% 80.89% at 86.05% 2.8%, #5621d5 0%, #040d1c 100%);
+    padding: 15%;
+    border-radius: 2em;
+    box-shadow: 0 0 2em rgba(0, 0, 0, 0.5);
+    color: white;
+    font-family: Arial, sans-serif;
+    @media (max-width: 810px) {
+      font-size: .7em;
+    }
+    width: 30%;
+    text-align: center;
+    min-width: 10%;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+  input,
+  select {
+    @media (max-width: 810px) {
+      font-size: .7em;
+    }
   }
-  .read-the-docs {
-    color: #888;
+
+  h1 {
+    color: #23dbb0;
+  }
+
+  .converter-main {
+    display: flex;
+    flex-direction: column;
+    row-gap: 1.3em;
+    background: linear-gradient(84deg, rgba(108, 43, 255, 0.3) 3.94%, rgba(121, 61, 255, 0) 99.55%);
+    padding: 3em;
+    border-radius: 2em;
+    border: 1px solid #5423ca;
+    width: 75%;
+  }
+
+  .titles,
+  .inputs {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    font-weight: bold;
+  }
+
+  .titles__result select {
+    font-weight: bold;
+
+  }
+  .inputs input {
+    outline: none;
+    padding: .5em 0;
+    border-radius: .3em;
+    text-align: center;
+    color: #23dbb0;
+    border: none;
+    font-weight: 600;
+    max-width: 40%;
   }
 </style>
